3afc676c360feb346599c132074078fd962f16bf
Ricardo Amador Xolo <ricardoamadorxolo@gmail.com>
2023-12-01 21:14:12 -0800

    Practica3

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useState, useEffect, Component } from 'react';
+import { ListGroupItem } from 'react-bootstrap';
 
-function App() {
+
+const App = () => {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    var url = 'https://fakestoreapi.com/products?limit=3';
+
+    fetch(url)
+      .then((data) => {
+          return data.json();
+      })
+      .then((response) => {
+       setProducts(response)
+     })
+
+  }, []);
+
   return (
-    <div className="App">
+    <div>
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <p className='Titulo'>FakeStore</p>
       </header>
+      <ul>
+        {products.map((product) => (
+          <li className='Productos' key={product.id}>
+            <h1>{product.title}</h1>
+            <p>{product.description}</p>
+            <p>${product.price} Dolares</p>
+            <div><img src={product.image} alt={product.title} style={{ maxWidth: '50px' }} /></div>
+          </li>
+        ))}
+      </ul>
     </div>
   );
-}
-
+};
 export default App;
